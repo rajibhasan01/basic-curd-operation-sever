71142bd1bed0992baa657ab9513c43e50eba85bc
unknown <hasan.rajib1996@gmail.com>
2021-12-14 03:04:58 +0600

    database connected

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+
+
+// database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nzlp2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
